dfa9106e9655138d9870685b805d297786408ba1
Roman <roman.fedanyak@lnu.edu.ua>
2023-03-09 21:09:30 +0200

    seccond_commit

---
+++ src/components/main/RecipeType.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./style.css";
+import "./../../helpers/data"
+
+const RecipeType = () => {
+    return(
+        <div>
+            RecipeType
+        </div>
+    )
+}
+
+export default Main;
+
+
+
+
+
+
+
