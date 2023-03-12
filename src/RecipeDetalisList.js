c56195e372772a39e48d20523c827098c88ce597
Roman <roman.fedanyak@lnu.edu.ua>
2023-03-12 21:48:51 +0200

    main_recipe_1

---
+++ src/RecipeDetalisList.js
@@ -0,0 +1,32 @@
+import {useState} from "react";
+import {dataa as defaultDataa} from "./components/RecipeList/RecipeListData";
+import RecipeList from "./components/RecipeList/RecipeList";
+import RecipeDetilesItem from "./components/RecipeDetiles/RecipeDetilesItem";
+import {data as defaultData} from "./helpers/data";
+
+function RecipeDetalisList(){
+
+    const [data, setData] = useState(defaultData);
+    const [dataa, setDataa] = useState(defaultDataa);
+    const [activeElements, setActiveElements] = useState([]);
+
+    return(
+        <div className="recipe_container">
+            <div className="recipe_list">
+                {
+                    dataa.map((value, index) => <RecipeList key={index} data={value} setActiveElements={setActiveElements}/> )
+                }
+            </div>
+
+            <div className="details_style">
+                <div className="types">
+                    {
+                        activeElements && activeElements.map((value, index)=> <RecipeDetilesItem data={value} key={index}  /> )
+                    }
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default RecipeDetalisList;
---
+++ src/components/RecipeList/RecipeListData.js
@@ -0,0 +1,91 @@
+
+export const dataa = [
+    {
+        id: 1,
+        name: "main",
+        category: "Desert",
+        image: "https://cdn.discordapp.com/attachments/1042803291500847156/1083490666065166376/1.png",
+        items: [
+            {
+                id: 1,
+                name: "Test te1st",
+                image: "https://i.imgur.com/oWZjjQF.png",
+                category: "Dessert",
+                timeForDone: "1 hour",
+                description: "Delicious and moist chocolate cake.",
+                ingredients: [
+                    "1 3/4 cups all-purpose flour",
+                    "3/4 cup cocoa powder",
+                    "1 3/4 cups granulated sugar",
+                    "2 teaspoons baking soda",
+                    "1 teaspoon baking powder",
+                    "1 teaspoon salt",
+                    "1 cup buttermilk",
+                    "1/2 cup vegetable oil",
+                    "2 large eggs",
+                    "2 teaspoons vanilla extract",
+                    "1 cup hot water",
+                ],
+                recipe:
+                    "Preheat oven to 350°F. Grease and flour two 9-inch round cake pans. In a large bowl, combine flour, cocoa, sugar, baking soda, baking powder, and salt. Add buttermilk, oil, eggs, and vanilla; beat on low speed for 30 seconds. Beat on high speed for 2 minutes. Stir in hot water (batter will be thin). Pour batter into prepared pans. Bake 30 to 35 minutes or until a toothpick inserted in the center comes out clean. Cool 10 minutes; remove from pans to wire racks. Cool completely.",
+            },
+
+        ]
+    },
+    {
+        id: 2,
+        name: "main",
+        image: "https://cdn.discordapp.com/attachments/1042803291500847156/1083490665817706638/2.png",
+        items: [
+            {
+                image: "https://i.imgur.com/oWZjjQF.png",
+                id: 1,
+                category: "Drinks",
+                timeForDone: "10 minutes",
+                description: "A refreshing lemonade with mint.",
+                ingredients: ["lemon", "water", "sugar", "mint"],
+                recipe: "1. Squeeze the juice from 4 lemons into a pitcher.\n2. Add 4 cups of water and 1 cup of sugar to the pitcher and stir until the sugar dissolves.\n3. Add a handful of fresh mint leaves to the pitcher and stir.\n4. Chill the lemonade in the refrigerator for at least 30 minutes before serving.",
+                name: "Lemonade with Mint"
+            },
+
+        ]
+    },
+    {
+        id: 3,
+        name: "main",
+        image: "https://cdn.discordapp.com/attachments/1042803291500847156/1083490665553473666/3.png",
+        items: [
+            {
+                image: "https://i.imgur.com/oWZjjQF.png",
+                id: 1,
+                category: "Main Dishs",
+                timeForDone: "45 minutes",
+                description: "Delicious pasta with creamy sauce",
+                ingredients: ["pasta", "heavy cream", "Parmesan cheese", "garlic", "olive oil"],
+                recipe: "1. Cook pasta according to the package instructions. Reserve 1 cup of pasta cooking water. \n2. In a saucepan, heat olive oil over medium heat. Add garlic and cook until fragrant. \n3. Add heavy cream and Parmesan cheese. Stir until the cheese has melted and the sauce is smooth. \n4. Add cooked pasta to the saucepan and stir to combine. \n5. Add reserved pasta cooking water as needed to thin out the sauce. \n6. Serve immediately and enjoy!",
+                name: "Creamy Parmesan Pasta"
+            },
+
+        ]
+    },
+    {
+        id: 4,
+        name: "main",
+        image: "https://cdn.discordapp.com/attachments/1042803291500847156/1083490665305997373/4.png",
+        items: [
+            {
+                image: "https://i.imgur.com/oWZjjQF.png",
+                id: 1,
+                category: "Side Dishs",
+                timeForDone: "30 minutes",
+                description: "Delicious roasted vegetables",
+                ingredients: ["carrots", "potatoes", "onions", "garlic"],
+                recipe: "1. Preheat the oven to 400°F (200°C). \n2. Cut the carrots and potatoes into bite-sized pieces. \n3. Slice the onions and chop the garlic. \n4. In a bowl, toss the vegetables with olive oil, salt, and pepper. \n5. Spread the vegetables in a single layer on a baking sheet. \n6. Roast in the oven for 20-25 minutes or until tender and golden brown.",
+                name: "Roasted Vegetables"
+            },
+
+        ]
+    },
+
+]
+
